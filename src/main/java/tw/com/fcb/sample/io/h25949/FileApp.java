3546c36498fe0bb7a1ff36405245cdb4dbb70b26
h25949 <h25949@gmail.com>
2022-02-05 16:42:47 +0800

    initialization

---
+++ src/main/java/tw/com/fcb/sample/io/h25949/FileApp.java
@@ -0,0 +1,10 @@
+package tw.com.fcb.sample.io.h25949;
+
+public class FileApp {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+
+	}
+
+}
---
+++ src/main/java/tw/com/fcb/sample/io/h25949/FinancialIndicatorsService.java
@@ -0,0 +1,5 @@
+package tw.com.fcb.sample.io.h25949;
+
+public class FinancialIndicatorsService {
+
+}
