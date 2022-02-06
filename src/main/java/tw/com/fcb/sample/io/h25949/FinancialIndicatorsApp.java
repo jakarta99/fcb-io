43ce95d9f0ef7234264f833061b2cfc7112aaba1
h25949 <h25949@gmail.com>
2022-02-06 22:03:11 +0800

    Finish v1

--- src/main/java/tw/com/fcb/sample/io/h25949/FinancialIndicatorsApp.java
+++ src/main/java/tw/com/fcb/sample/io/h25949/FinancialIndicatorsApp.java
@@ -1,10 +1,25 @@
 package tw.com.fcb.sample.io.h25949;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class FinancialIndicatorsApp {
 
 	public static void main(String[] args) {
 		// 提供每年國內主要金融指標
-		
+		try {
+			List<FinancialIndicators> result = new FinancialIndicatorsService().FinancialIndicatorsFile();
+			System.out.println("目前已經有 " + result.size() + "筆資料");
+		} catch (FileNotFoundException e) {
+			System.out.println("找不到檔案");
+		} catch (IOException e) {
+			System.out.println("檔案已毀損");
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+		}
 	}
 
 }
