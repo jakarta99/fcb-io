ea851de38c649f11535359cc4ba65a417611f1a2
jilldolala25 <jilldolala25@gmail.com>
2022-02-08 19:03:38 +0800

    db homework

--- src/main/java/tw/com/fcb/sample/io/jilldolala25/MaskMedicalService.java
+++ src/main/java/tw/com/fcb/sample/io/jilldolala25/MaskMedicalService.java
@@ -1,17 +1,27 @@
 package tw.com.fcb.sample.io.jilldolala25;
 
+import javax.print.attribute.standard.PrinterMakeAndModel;
 import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MaskMedicalService {
     List<MaskMedical> maskList = null;
+    MaskMedicalRepository maskMedicalRepository = new MaskMedicalRepository();
+    Connection conn = maskMedicalRepository.getConnection();
 
-    public void LoadMedicalFile() throws IOException {
+    public MaskMedicalService() throws SQLException {
+    }
+
+    public void LoadMedicalFile() throws IOException, SQLException {
         String inputFile = "c:\\data\\maskdata.csv";
         BufferedReader fileInputStream = new BufferedReader(new FileReader(inputFile));
         String lineData;
         maskList = new ArrayList<>();
+        maskMedicalRepository.delete(conn);
         int count = 0;
         while ((lineData = fileInputStream.readLine()) != null) {
             count++;
@@ -31,7 +41,14 @@ public class MaskMedicalService {
 //            System.out.println(maskMedical);
 
             maskList.add(maskMedical);
+
+
+            maskMedicalRepository.insert(maskMedical,conn);
+
         }
+        System.out.println("MaskMedicalRepository select 5907360379= "
+                + maskMedicalRepository.getByMedicalCode("5907360379"));
+        conn.close();
         System.out.println("目前共有"+ maskList.size()+"筆 健保特約機構");
 
     }
