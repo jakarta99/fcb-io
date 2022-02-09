a7d19cc5fe292ecf3ecd611dd5e877740ae3ddad
ijoshua29 <83047238+ijoshua29@users.noreply.github.com>
2022-02-09 16:18:24 +0800

    Access DB homework

--- src/main/java/tw/com/fcb/sample/io/ijoshua29/BankRoeApp.java
+++ src/main/java/tw/com/fcb/sample/io/ijoshua29/BankRoeApp.java
@@ -11,6 +11,9 @@ public class BankRoeApp {
 		// TODO Auto-generated method stub
 		BankRoeService bankRoeService = new BankRoeService();
 		List<BankRoe> listData = new ArrayList<BankRoe>();
+		int successUpdateResult = 0;
+		int faulureUpdateResult = 0;
+		boolean isWriteOK;
 		
 		try {
 			listData = bankRoeService.loadFromFile();
@@ -21,6 +24,31 @@ public class BankRoeApp {
 			{
 				System.out.println(listData.get(i).toString());				
 			}
+			
+			//insert db
+			successUpdateResult = bankRoeService.insertDb(listData);
+			//計算失敗筆數
+			faulureUpdateResult = listData.size() - successUpdateResult;
+			
+			System.out.println("Update DB結果:成功" + successUpdateResult +
+					"筆，失敗" + faulureUpdateResult + "筆");
+			
+			//從DB讀取資料
+			List<BankRoe> fetchData = new ArrayList<BankRoe>();
+			fetchData = bankRoeService.fetchDb();
+			
+			//寫出至CSV檔案
+			isWriteOK = bankRoeService.writeFile(fetchData);
+			
+			if (isWriteOK)
+			{
+				System.out.println("寫入檔案outBankRoa_e.csv成功");
+			}
+			else
+			{
+				System.out.println("寫入檔案outBankRoa_e.csv失敗");
+			}
+			
 		} 
 		catch (FileNotFoundException e) 
 		{
--- src/main/java/tw/com/fcb/sample/io/ijoshua29/BankRoeService.java
+++ src/main/java/tw/com/fcb/sample/io/ijoshua29/BankRoeService.java
@@ -1,8 +1,16 @@
 package tw.com.fcb.sample.io.ijoshua29;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,5 +44,139 @@ public class BankRoeService {
 		}
 
 		return listData;
+	}
+	
+	
+	private Connection getConnection() throws SQLException {
+		//Class.forName("org.postgresql.Driver");
+		String dbUrl = "jdbc:postgresql://localhost:5432/testdb";
+		String username = "postgres";
+		String password = "postgres";
+		return DriverManager.getConnection(dbUrl, username, password);
+			
+	}
+	
+	public int insertDb(List<BankRoe> listData) throws SQLException {
+		
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		int sqlResult = 0;
+		int successSqlResult = 0;
+		
+		for (int i = 0; i < listData.size(); i ++)
+		{
+			System.out.println(listData.get(i).toString());	
+			String sqlCmd = "INSERT INTO bankRoe values ("
+					+ listData.get(i).getStatisticalYear() + ", "
+					+ listData.get(i).getStatisticalMonth() + ", "
+					+ listData.get(i).getAverageAsset() + ", "
+					+ listData.get(i).getAverageNetWorth() + ", "
+					+ listData.get(i).getEbt() + ", "
+					+ listData.get(i).getRoa() + ", "
+					+ listData.get(i).getRoe()
+					+ ")";
+			
+			sqlResult = stmt.executeUpdate(sqlCmd);
+			
+			if (sqlResult == 1)
+				successSqlResult++;
+		}
+		
+		stmt.close();
+		conn.close();
+		return successSqlResult;
+	}
+	
+	public List<BankRoe> fetchDb() throws SQLException
+	{		
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		String sqlCmd = "select * from bankroe";
+		ResultSet rs = stmt.executeQuery(sqlCmd);
+		List<BankRoe> listData = new ArrayList<BankRoe>();
+		
+		while(rs.next())
+		{
+			BankRoe bankRoe = new BankRoe();
+			bankRoe.setStatisticalYear(rs.getInt("statisticalYear"));
+			bankRoe.setStatisticalMonth(rs.getInt("statisticalMonth"));
+			bankRoe.setAverageAsset(rs.getInt("averageAsset"));
+			bankRoe.setAverageNetWorth(rs.getInt("averageNetWorth"));
+			bankRoe.setEbt(rs.getInt("ebt"));
+			bankRoe.setRoa(rs.getDouble("roa"));
+			bankRoe.setRoe(rs.getDouble("roe"));
+			listData.add(bankRoe);
+		}
+		
+		stmt.close();
+		conn.close();
+		return listData;
+	}
+	
+	public boolean writeFile(List<BankRoe> fetchData) 
+	{		
+		boolean isWriteOK = true;
+		
+		try
+		{			
+			//建立檔案
+			String filePath = "D:\\outBankRoa_e.csv";
+			File file = new File(filePath);
+			file.createNewFile();
+			
+			FileWriter fw = new FileWriter(filePath, true);
+			StringBuilder sb = new StringBuilder();
+			
+			//寫title
+			sb.append("統計年度");
+			sb.append(",");
+			sb.append("統計月份");
+			sb.append(",");
+			sb.append("平均資產-新臺幣億元");
+			sb.append(",");
+			sb.append("平均淨值-新臺幣億元");
+			sb.append(",");
+			sb.append("稅前盈餘-新臺幣億元");
+			sb.append(",");
+			sb.append("資產報酬率(ROA)-%");
+			sb.append(",");
+			sb.append("淨值報酬率(ROE)-%");
+			sb.append("\n");
+			
+			for (int i = 0; i < fetchData.size(); i ++)
+			{
+				sb.append(fetchData.get(i).getStatisticalYear());
+				sb.append(",");
+				sb.append(fetchData.get(i).getStatisticalMonth());
+				sb.append(",");
+				sb.append(fetchData.get(i).getAverageAsset());
+				sb.append(",");
+				sb.append(fetchData.get(i).getAverageNetWorth());
+				sb.append(",");
+				sb.append(fetchData.get(i).getEbt());
+				sb.append(",");
+				sb.append(fetchData.get(i).getRoa());
+				sb.append(",");
+				sb.append(fetchData.get(i).getRoe());
+				sb.append("\n");
+			}
+			
+			fw.write(sb.toString());
+			fw.close();
+		}
+		catch (IOException e) 
+		{
+			System.out.println("檔案存取有誤");
+			e.printStackTrace();
+			isWriteOK = false;
+		} 
+		catch (Exception e) 
+		{
+			System.out.println("其他錯誤");
+			e.printStackTrace();
+			isWriteOK = false;
+		}
+		
+		return isWriteOK;
 	}
 }
