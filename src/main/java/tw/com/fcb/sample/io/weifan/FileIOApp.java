1ec001b87b7e2f8b4c92e57590406efc0d5948b2
DESKTOP-92P1581\weifan <a24562327@gmail.com>
2022-02-10 23:54:57 +0800

    update

--- src/main/java/tw/com/fcb/sample/io/weifan/FileIOApp.java
+++ src/main/java/tw/com/fcb/sample/io/weifan/FileIOApp.java
@@ -12,24 +12,25 @@ public class FileIOApp {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		/*List<Language> result = new LanguageService().loadFromFile();
+		List<Language> result = new LanguageService().loadFromFile();
 		for(int i = 0; i < result.size(); i++){
 			System.out.println(result.get(i).toString());
-		}*/
+		}
 		
-		String dbUrl = "jdbc:postgresql://localhost:5432/testdb";
-		String username = "postgres";
-		String password = "postgres";
 		try {
-			Connection conn = DriverManager.getConnection(dbUrl, username, password);
-			Statement stmt = conn.createStatement();
+			DataBaseService DB = new DataBaseService();
+			Statement stmt = DB.ConnectDB();
+			for(int i = 0; i < result.size(); i++){
+				String sqlCmd = result.get(i).toSqlcmd();
+			//	System.out.println(result.get(i).toSqlcmd());
+				stmt.executeUpdate(sqlCmd);
+			}
 			String sqlCmd = "select * from fruit";
 			ResultSet rs = stmt.executeQuery(sqlCmd);
 			while(rs.next()) {
 			System.out.println(rs.getString("code"));
 			System.out.println(rs.getString("name"));
 			System.out.println(rs.getString("price"));
-			conn.close();
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
