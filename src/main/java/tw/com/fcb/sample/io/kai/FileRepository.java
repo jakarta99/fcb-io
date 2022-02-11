f6efacdd0ba5b9eb6e7c16cfc533157e6c4cce0c
LukeKaiQQ <https://github.com/LukeKaiQQ>
2022-02-12 01:03:43 +0800

    commit repository

--- src/main/java/tw/com/fcb/sample/io/kai/FileRepository.java
+++ src/main/java/tw/com/fcb/sample/io/kai/FileRepository.java
@@ -1,120 +1,170 @@
 package tw.com.fcb.sample.io.kai;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class FileRepository {
-	public Connection getConnection() throws SQLException {
+	List<FileSecurities> list = null;
+	FileSecurities fileSecurities = null;
+	
+	// getConnection()
+	public Connection getConnection() throws Exception {
 		Properties props = new Properties();
 		String dbUrl = null, username = null, password = null;
 		
-		try {
-//			String dbUrl = "jdbc:postgresql://localhost:5432/testdb";
-//			String username = "postgres";
-//			String password = "postgres";
-			props.load(new FileInputStream("jdbc.properties"));
-			dbUrl = props.getProperty("dbUrl");
-			username = props.getProperty("username");
-			password = props.getProperty("password");
-		}
-		catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
+//		String dbUrl = "jdbc:postgresql://localhost:5432/testdb";
+//		String username = "postgres";
+//		String password = "postgres";
+		props.load(new FileInputStream("jdbc.properties"));
+		dbUrl = props.getProperty("dbUrl");
+		username = props.getProperty("username");
+		password = props.getProperty("password");
 		
 		return DriverManager.getConnection(dbUrl, username, password);
 	}
 	
-	public void selectData() {
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-		
-		try {
-			connection = getConnection();
-			statement = connection.createStatement();
-			
-			String selectSql = "SELECT * FROM stock";
-			resultSet = statement.executeQuery(selectSql);
-			
-			ResultSetMetaData rMetaData = resultSet.getMetaData();
-			int columns = rMetaData.getColumnCount();
-			for(int i = 1 ; i <= columns ; i++)
-				System.out.printf("%-8s\t", rMetaData.getColumnName(i));
-			System.out.println();
-			while(resultSet.next()) {
-				for(int i = 1 ; i <= columns ; i++)
-					System.out.printf("%-15s\t", resultSet.getObject(i));
-				System.out.println();
-			}
-			
-//			while(resultSet.next()) {
-//				System.out.print(resultSet.getString("stockorder") + "\t");
-//				System.out.print(resultSet.getString("stockcode") + "\t");
-//				System.out.print(resultSet.getString("stockname") + "\t");
-//				System.out.print(resultSet.getString("stocktransaction") + "\t");
-//				System.out.print(resultSet.getString("etfcode") + "\t");
-//				System.out.print(resultSet.getString("etfname") + "\t");
-//				System.out.print(resultSet.getString("etftransaction") + "\t");
-//				System.out.println();
-//			}
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public ResultSet selectOrderData() {
-		Connection connection = null;
-		Statement statement = null;
-		ResultSet resultSet = null;
-		
-		try {
-			connection = getConnection();
-			statement = connection.createStatement();
-			
-			String selectSql = "SELECT * FROM stock ORDER BY stockcode";
-			resultSet = statement.executeQuery(selectSql);
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return resultSet;
-	}
-	
-	public void insertData(FileSecurities fileSecurities) {
+	// findAll()
+	public List<FileSecurities> findAll() throws Exception {
 		Connection connection = null;
 		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
 		
-		try {
-			connection = getConnection();
+		connection = getConnection();
 			
-			String insertSql = "INSERT INTO stock VALUES(?, ?, ?, ?, ?, ?, ?)";
-			pStatement = connection.prepareStatement(insertSql);
-			pStatement.setString(1, fileSecurities.getSecuritiesOrder());
-			pStatement.setString(2, fileSecurities.getStockCode());
-			pStatement.setString(3, fileSecurities.getStockName());
-			pStatement.setString(4, fileSecurities.getStockTransaction());
-			pStatement.setString(5, fileSecurities.getEtfCode());
-			pStatement.setString(6, fileSecurities.getEtfName());
-			pStatement.setString(7, fileSecurities.getEtfTransaction());
-			pStatement.executeUpdate();
-			pStatement.clearParameters();
+		String selectSql = "SELECT * FROM stock";
+		pStatement = connection.prepareStatement(selectSql);
+		resultSet = pStatement.executeQuery();
+		
+		list = new ArrayList<FileSecurities>();
+		while(resultSet.next()) {
+			fileSecurities = new FileSecurities();
+			fileSecurities.setSecuritiesOrder(resultSet.getString("stockorder"));
+			fileSecurities.setStockCode(resultSet.getString("stockcode"));
+			fileSecurities.setStockName(resultSet.getString("stockname"));
+			fileSecurities.setStockTransaction(resultSet.getString("stocktransaction"));
+			fileSecurities.setEtfCode(resultSet.getString("etfcode"));
+			fileSecurities.setEtfName(resultSet.getString("etfname"));
+			fileSecurities.setEtfTransaction(resultSet.getString("etftransaction"));
+			list.add(fileSecurities);
 		}
-		catch (SQLException e) {
-			e.printStackTrace();
+		
+		return list ;
+	}
+	
+	// findAllOrderData()
+	public List<FileSecurities> findAllOrderData() throws Exception {
+		Connection connection = null;
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		
+		connection = getConnection();
+			
+		String selectSql = "SELECT * FROM stock ORDER BY stockcode";
+		pStatement = connection.prepareStatement(selectSql);
+		resultSet = pStatement.executeQuery();
+		
+		list = new ArrayList<FileSecurities>();
+		while(resultSet.next()) {
+			fileSecurities = new FileSecurities();
+			fileSecurities.setSecuritiesOrder(resultSet.getString("stockorder"));
+			fileSecurities.setStockCode(resultSet.getString("stockcode"));
+			fileSecurities.setStockName(resultSet.getString("stockname"));
+			fileSecurities.setStockTransaction(resultSet.getString("stocktransaction"));
+			fileSecurities.setEtfCode(resultSet.getString("etfcode"));
+			fileSecurities.setEtfName(resultSet.getString("etfname"));
+			fileSecurities.setEtfTransaction(resultSet.getString("etftransaction"));
+			list.add(fileSecurities);
 		}
+		
+		return list ;
+	}
+	
+	// findById()
+	public List<FileSecurities> findById(String stockOrder) throws Exception {
+		Connection connection = null;
+		PreparedStatement pStatement = null;
+		ResultSet resultSet = null;
+		
+		connection = getConnection();
+			
+		String selectSql = "SELECT * FROM stock WHERE stockOrder = ?";
+		pStatement = connection.prepareStatement(selectSql);
+		pStatement.setString(1, stockOrder);
+		resultSet = pStatement.executeQuery();
+		
+		list = new ArrayList<FileSecurities>();
+		if(resultSet.next()) {
+			fileSecurities = new FileSecurities();
+			fileSecurities.setSecuritiesOrder(resultSet.getString("stockorder"));
+			fileSecurities.setStockCode(resultSet.getString("stockcode"));
+			fileSecurities.setStockName(resultSet.getString("stockname"));
+			fileSecurities.setStockTransaction(resultSet.getString("stocktransaction"));
+			fileSecurities.setEtfCode(resultSet.getString("etfcode"));
+			fileSecurities.setEtfName(resultSet.getString("etfname"));
+			fileSecurities.setEtfTransaction(resultSet.getString("etftransaction"));
+			list.add(fileSecurities);
+		}
+		else {
+			System.out.println("The data of stockOrder = " + stockOrder + " not found!! ");
+		}
+		
+		return list ;
+	}
+	
+	// insert
+	public void insert(FileSecurities fileSecurities) throws Exception {
+		Connection connection = null;
+		PreparedStatement pStatement = null;
+
+		connection = getConnection();
+			
+		String insertSql = "INSERT INTO stock VALUES(?, ?, ?, ?, ?, ?, ?)";
+		pStatement = connection.prepareStatement(insertSql);
+		pStatement.setString(1, fileSecurities.getSecuritiesOrder());
+		pStatement.setString(2, fileSecurities.getStockCode());
+		pStatement.setString(3, fileSecurities.getStockName());
+		pStatement.setString(4, fileSecurities.getStockTransaction());
+		pStatement.setString(5, fileSecurities.getEtfCode());
+		pStatement.setString(6, fileSecurities.getEtfName());
+		pStatement.setString(7, fileSecurities.getEtfTransaction());
+		pStatement.executeUpdate();
+		pStatement.clearParameters();
+	}
+	
+	// update
+	public void update(String stockCode, String stockOrder) throws Exception {
+		Connection connection = null;
+		PreparedStatement pStatement = null;
+
+		connection = getConnection();
+			
+		String updateSql = "UPDATE stock SET stockcode = ? WHERE stockorder = ?";
+		pStatement = connection.prepareStatement(updateSql);
+		pStatement.setString(1, stockCode);
+		pStatement.setString(2, stockOrder);
+		
+		int affectedRow = pStatement.executeUpdate();
+		System.out.println("共 " + affectedRow + " 筆資料更新");
+	}
+	
+	// delete
+	public void delete(String stockOrder) throws Exception {
+		Connection connection = null;
+		PreparedStatement pStatement = null;
+
+		connection = getConnection();
+			
+		String updateSql = "DELETE FROM stock WHERE stockorder = ?";
+		pStatement = connection.prepareStatement(updateSql);
+		pStatement.setString(1, stockOrder);
+		
+		int affectedRow = pStatement.executeUpdate();
+		System.out.println("共 " + affectedRow + " 筆資料刪除");
 	}
 }
--- src/main/java/tw/com/fcb/sample/io/kai/FileSecuritiesApp.java
+++ src/main/java/tw/com/fcb/sample/io/kai/FileSecuritiesApp.java
@@ -1,10 +1,105 @@
 package tw.com.fcb.sample.io.kai;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class FileSecuritiesApp {
 	public static void main(String[] args) {
 		FileSecuritiesService fileSecuritiesService = new FileSecuritiesService();
-		fileSecuritiesService.readFile();
-		fileSecuritiesService.selectData();
-		fileSecuritiesService.writeFile();
+		FileSecurities fileSecurities = null;
+		
+		Scanner scanner = new Scanner(System.in);
+		try {
+			while(true) {
+				System.out.print("Enter the option: 1).read-file 2).write-file 3).findAll 4).findById 5).insert 6).update 7).delete -1)Quit: ");
+				int option = scanner.nextInt();
+				
+				if(option == 1) {
+					fileSecuritiesService.readFile();
+				}
+				else if(option == 2) {
+					fileSecuritiesService.writeFile();
+				}
+				else if(option == 3) {
+					fileSecuritiesService.findAll();
+				}
+				else if(option == 4) {
+					System.out.print("Enter the Stock Order : ");
+					String stockOrder = scanner.next();
+					fileSecuritiesService.findById(stockOrder);
+				}
+				else if(option == 5) {
+					fileSecurities = new FileSecurities();
+					System.out.print("Enter the stockOrder: ");
+					String stockOrder = scanner.next();
+					fileSecurities.setSecuritiesOrder(stockOrder);
+					System.out.print("Enter the stockCode: ");
+					String stockCode = scanner.next();
+					fileSecurities.setStockCode(stockCode);
+					System.out.print("Enter the stockName: ");
+					String stockName = scanner.next();
+					fileSecurities.setStockName(stockName);
+					System.out.print("Enter the stockTrans: ");
+					String stockTrans = scanner.next();
+					fileSecurities.setStockTransaction(stockTrans);
+					System.out.print("Enter the etfCode: ");
+					String etfCode = scanner.next();
+					fileSecurities.setEtfCode(etfCode);
+					System.out.print("Enter the etfName: ");
+					String etfName = scanner.next();
+					fileSecurities.setEtfName(etfName);
+					System.out.print("Enter the etfTrans: ");
+					String etfTrans = scanner.next();
+					fileSecurities.setEtfTransaction(etfTrans);
+					
+					fileSecuritiesService.insert(fileSecurities);
+				}
+				else if(option == 6) {
+					System.out.print("Enter the stockCode: ");
+					String stockCode = scanner.next();
+					System.out.print("Enter the stockOrder: ");
+					String stockOrder = scanner.next();
+					
+					fileSecuritiesService.update(stockCode, stockOrder);
+				}
+				else if(option == 7) {
+					System.out.print("Enter the stockOrder: ");
+					String stockOrder = scanner.next();
+					
+					fileSecuritiesService.delete(stockOrder);
+				}
+				else if(option == -1) {
+					System.out.println("Quit...");
+					break;
+				}
+				else {
+					System.out.println("Wrong Option!!! ");
+				}
+				System.out.println();
+			}
+		} 
+		catch(Exception e) {
+			if(e instanceof SQLException) {
+				e.printStackTrace();
+				System.out.println("資料庫連線有誤");
+			}
+			else if(e instanceof FileNotFoundException) {
+				e.printStackTrace();
+				System.out.println("檔案不存在");
+			}
+			else if(e instanceof IOException) {
+				e.printStackTrace();
+				System.out.println("檔案讀取有誤");
+			}
+			else {
+				e.printStackTrace();
+				System.out.println("未知例外");
+			}
+		}
+		finally {
+			scanner.close();
+		}
 	}
 }
