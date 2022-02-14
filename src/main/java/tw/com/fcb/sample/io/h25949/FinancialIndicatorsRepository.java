5ea6fcc5d59a192e115d0ea653cc1a9b992153f4
h25949 <h25949@gmail.com>
2022-02-14 17:54:37 +0800

    CRUD v3

--- src/main/java/tw/com/fcb/sample/io/h25949/FinancialIndicatorsRepository.java
+++ src/main/java/tw/com/fcb/sample/io/h25949/FinancialIndicatorsRepository.java
@@ -21,39 +21,40 @@ public class FinancialIndicatorsRepository {
 	public int insert(List<FinancialIndicators> result) throws SQLException {
 
 		Connection conn = getConnection();
-		int count=0;
-		
+		int count = 0;
+
 		for (FinancialIndicators FinancialIndicator : result) {
-			String SQL="insert into fruit(the_year,exchange_rate,the_foreign,stock_index,stock_amount) values(?,?,?,?,?)";
-			PreparedStatement ps= conn.prepareStatement(SQL);
-			
-			ps.setInt(1,FinancialIndicator.getYear());
-			ps.setBigDecimal(2,FinancialIndicator.getExchangeRate());
-			ps.setBigDecimal(3,FinancialIndicator.getForeign());
-			ps.setBigDecimal(4,FinancialIndicator.getStockIndex());
-			ps.setBigDecimal(5,FinancialIndicator.getStockAmount());
-			
-			count = count + ps.executeUpdate();;
+			String SQL = "insert into fruit(the_year,exchange_rate,the_foreign,stock_index,stock_amount) values(?,?,?,?,?)";
+			PreparedStatement ps = conn.prepareStatement(SQL);
+
+			ps.setInt(1, FinancialIndicator.getYear());
+			ps.setBigDecimal(2, FinancialIndicator.getExchangeRate());
+			ps.setBigDecimal(3, FinancialIndicator.getForeign());
+			ps.setBigDecimal(4, FinancialIndicator.getStockIndex());
+			ps.setBigDecimal(5, FinancialIndicator.getStockAmount());
+
+			count = count + ps.executeUpdate();
+			;
 		}
 		return count;
 	}
-	
-	public List<FinancialIndicators> findAll() throws SQLException{
-		Connection conn = getConnection();	
+
+	public List<FinancialIndicators> findAll() throws SQLException {
+		Connection conn = getConnection();
 		String sqlCmd = "select * from fruit";
-		PreparedStatement ps= conn.prepareStatement(sqlCmd);
+		PreparedStatement ps = conn.prepareStatement(sqlCmd);
 		ResultSet rs = ps.executeQuery();
-		
+
 		List<FinancialIndicators> FinancialIndicators = new ArrayList<FinancialIndicators>();
 		FinancialIndicators FinancialIndicator;
-		while(rs.next()) {
+		while (rs.next()) {
 			FinancialIndicator = new FinancialIndicators();
 			FinancialIndicator.setYear(rs.getInt("the_year"));
 			FinancialIndicator.setExchangeRate(rs.getBigDecimal("exchange_rate"));
 			FinancialIndicator.setForeign(rs.getBigDecimal("the_foreign"));
 			FinancialIndicator.setStockIndex(rs.getBigDecimal("stock_index"));
 			FinancialIndicator.setStockAmount(rs.getBigDecimal("stock_amount"));
-			
+
 			FinancialIndicators.add(FinancialIndicator);
 		}
 		return FinancialIndicators;
@@ -62,9 +63,9 @@ public class FinancialIndicatorsRepository {
 	public FinancialIndicators getById(int optionById) throws SQLException {
 		Connection conn = getConnection();
 		String sqlCmd = "select * from fruit where the_year = ?";
-		PreparedStatement ps= conn.prepareStatement(sqlCmd);
-		
-		ps.setInt(1,optionById);
+		PreparedStatement ps = conn.prepareStatement(sqlCmd);
+
+		ps.setInt(1, optionById);
 		ResultSet rs = ps.executeQuery();
 
 		FinancialIndicators FinancialIndicator = new FinancialIndicators();
@@ -79,29 +80,28 @@ public class FinancialIndicatorsRepository {
 		return FinancialIndicator;
 	}
 
-	public int update(FinancialIndicators financialIndicator) throws SQLException{
-		String SQL="UPDATE fruit SET exchange_rate=? , the_foreign=?, stock_index=?,stock_amount =? WHERE the_year = ?";
+	public int update(FinancialIndicators financialIndicator) throws SQLException {
+		String SQL = "UPDATE fruit SET exchange_rate=? , the_foreign=?, stock_index=?,stock_amount =? WHERE the_year = ?";
 		Connection conn = getConnection();
-		PreparedStatement ps= conn.prepareStatement(SQL);
-		
-		ps.setBigDecimal(1,financialIndicator.getExchangeRate());
-		ps.setBigDecimal(2,financialIndicator.getForeign());
-		ps.setBigDecimal(3,financialIndicator.getStockIndex());
-		ps.setBigDecimal(4,financialIndicator.getStockAmount());
-		ps.setInt(5,financialIndicator.getYear());
+		PreparedStatement ps = conn.prepareStatement(SQL);
+
+		ps.setBigDecimal(1, financialIndicator.getExchangeRate());
+		ps.setBigDecimal(2, financialIndicator.getForeign());
+		ps.setBigDecimal(3, financialIndicator.getStockIndex());
+		ps.setBigDecimal(4, financialIndicator.getStockAmount());
+		ps.setInt(5, financialIndicator.getYear());
 
 		return ps.executeUpdate();
 	}
 
-	public int delete(int optionById) throws SQLException{
+	public int delete(int optionById) throws SQLException {
 		String SQL = "DELETE FROM fruit WHERE the_year = ?";
 		Connection conn = getConnection();
-		PreparedStatement ps= conn.prepareStatement(SQL);
-		
-		ps.setInt(1,optionById);
-		
+		PreparedStatement ps = conn.prepareStatement(SQL);
+
+		ps.setInt(1, optionById);
+
 		return ps.executeUpdate();
 	}
 
-	
 }
