22cf2bd37f04126cea57c3228348082e6c4ccb71
HsiehCJ <playstar507@gmail.com>
2022-02-15 01:01:47 +0800

    commit

--- src/main/java/tw/com/fcb/sample/io/cj/VehicleRepository.java
+++ src/main/java/tw/com/fcb/sample/io/cj/VehicleRepository.java
@@ -27,15 +27,18 @@ public class VehicleRepository {
         String sqlCmd = "select * from vehiclesharing where year = '" + year + "'";
         ResultSet rs = stmt.executeQuery(sqlCmd);
 
+
+
         List<VehicleSharing> vehicleSharingList = new ArrayList<VehicleSharing>();
         while (rs.next()) {
             VehicleSharing vehicleSharing = new VehicleSharing();
+            vehicleSharing.setId(rs.getInt("id"));
             vehicleSharing.setYear(rs.getInt("year"));
             vehicleSharing.setMonth(rs.getInt("month"));
             vehicleSharing.setBrand(rs.getString("brand"));
             vehicleSharing.setType(rs.getInt("type"));
             vehicleSharing.setAmount(rs.getInt("amount"));
-//            System.out.println(vehicleSharing);
+//            System.out.println("!!!"+vehicleSharing);
             vehicleSharingList.add(vehicleSharing);
 //            System.out.println(vehicleSharingList);
         }
@@ -44,7 +47,7 @@ public class VehicleRepository {
 
     public static int insert(VehicleSharing vehicleSharing) throws SQLException {
 
-        String sqlCmd = "INSERT INTO vehiclesharing values ("+vehicleSharing.getYear()+", "
+        String sqlCmd = "INSERT INTO vehiclesharing (year, month, brand, type, amount) values ("+vehicleSharing.getYear()+", "
                 +     vehicleSharing.getMonth()+" , "
                 + "'"+vehicleSharing.getBrand()+"', "
                 +     vehicleSharing.getType()+" , "
@@ -63,6 +66,70 @@ public class VehicleRepository {
 
         return -1;
 
+    }
+
+    public static List<VehicleSharing> getByID(int id) throws SQLException {
+
+        String sqlCmd = "select * from vehiclesharing where id = '" + id + "'";
+        ResultSet rs = stmt.executeQuery(sqlCmd);
+
+
+
+        List<VehicleSharing> vehicleSharingList = new ArrayList<VehicleSharing>();
+        while (rs.next()) {
+            VehicleSharing vehicleSharing = new VehicleSharing();
+            vehicleSharing.setId(rs.getInt("id"));
+            vehicleSharing.setYear(rs.getInt("year"));
+            vehicleSharing.setMonth(rs.getInt("month"));
+            vehicleSharing.setBrand(rs.getString("brand"));
+            vehicleSharing.setType(rs.getInt("type"));
+            vehicleSharing.setAmount(rs.getInt("amount"));
+//            System.out.println("!!!"+vehicleSharing);
+            vehicleSharingList.add(vehicleSharing);
+//            System.out.println(vehicleSharingList);
+        }
+        return vehicleSharingList;
+    }
+
+    public static List<VehicleSharing> findAll() throws SQLException {
+
+        String sqlCmd = "select * from vehiclesharing;";
+        ResultSet rs = stmt.executeQuery(sqlCmd);
+
+
+        List<VehicleSharing> vehicleSharingList = new ArrayList<VehicleSharing>();
+        while (rs.next()) {
+            VehicleSharing vehicleSharing = new VehicleSharing();
+            vehicleSharing.setId(rs.getInt("id"));
+            vehicleSharing.setYear(rs.getInt("year"));
+            vehicleSharing.setMonth(rs.getInt("month"));
+            vehicleSharing.setBrand(rs.getString("brand"));
+            vehicleSharing.setType(rs.getInt("type"));
+            vehicleSharing.setAmount(rs.getInt("amount"));
+//            System.out.println("!!!"+vehicleSharing);
+            vehicleSharingList.add(vehicleSharing);
+//            System.out.println(vehicleSharingList);
+        }
+        return vehicleSharingList;
+    }
+
+    public static int update(int id, int amount) throws SQLException {
+
+        String sqlCmd = "UPDATE vehiclesharing" +
+                " SET amount = " + amount +
+                " WHERE id = "+ id + ";" ;
+        Integer result = stmt.executeUpdate(sqlCmd);
+
+        return result;
+    }
+
+    public static int delete(int id) throws SQLException {
+
+        String sqlCmd = "DELETE FROM vehiclesharing" +
+                " WHERE id = " + id + " ;" ;
+        Integer result = stmt.executeUpdate(sqlCmd);
+
+        return result;
     }
 
 }
